8b50e4ca6f80d965f41d9577298193a78a287bc4
BoneCapsule <161250058@smail.nju.edu.cn>
2020-08-15 16:18:34 +0800

    Initial project configuration

---
+++ app/src/main/java/ljie/coolweather/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package ljie.coolweather.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ljie.coolweather.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/ljie/coolweather/activity/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package ljie.coolweather.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ljie.coolweather.R
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
